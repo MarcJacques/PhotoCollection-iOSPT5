1e39790f772c5c473503ae82096a43bab734cd4e
Marc Jacques <Marcjacques@me.com>
2020-02-20 23:51:55 -0500

    Initial Commit

---
+++ PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Marc Jacques on 2/20/20.
+//  Copyright © 2020 Marc Jacques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
